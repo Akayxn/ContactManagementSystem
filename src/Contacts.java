ff2ab854e90c6f2fbea182e1e755e632fc1d7987
Yujan Bajagain <98070107+GamingClaus@users.noreply.github.com>
2025-01-27 21:30:52 +0545

    First Push

---
+++ src/Contacts.java
@@ -0,0 +1,48 @@
+public class Contacts {
+    private String name;
+    private long phoneNumber;
+    private String email;
+
+    // Constructor
+    Contacts(){
+        name = "No Name";
+        phoneNumber = 1122334455;
+        email = "noname@gmail.com";
+    }
+
+    Contacts(String name, long phoneNumber, String email){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // getters and setters methods.
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + "\nPhoneNumber: " + phoneNumber + "\nEmail: " + email;
+    }
+}
